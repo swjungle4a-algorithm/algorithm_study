a05c831adc781ac646ac81762b40f63ff2dbbecf
Jack <95907671+JackLee9437@users.noreply.github.com>
2022-05-03 21:55:22 +0900

    Update 거리두기 확인하기.py
    해결....
    동시다발로 bfs 진행시
    -> 방문확인이 어려운 점 : 출발점인 P도 도착지가 될 수 있음 - 문제는 자기자신에서 출발해서 돌아오는것까지도 2 안에 있는것으로 detect 해버림
    => 가능한 모든 출발점 P들에서 하나씩 빼서 bfs돌림 - 출발지만 방문체크하여 방문하지 못하도록 하고, 다른 P들은 방문할 수 있되 거리 2 이하로 방문하는 경우에는 answer에 0 입력하고 break. 해결됨,,

--- jack/lv2/거리두기 확인하기.py
+++ jack/lv2/거리두기 확인하기.py
@@ -7,31 +7,31 @@ def solution(places):
     dc = [0, 0, 1, -1]
     
     for place in places :
-        que = deque()
-        visit = [[-1] * 5 for _ in range(5)]
+        que1 = deque()
         for i in range(5) :
             for j in range(5) :
                 if place[i][j] == "P" :
-                    que.append((i,j))
-                    visit[i][j] = 0
+                    que1.append((i,j))
         brkFlag = False
-        while que :
-            r, c = que.popleft()
-            for i in range(4) :
-                nr = r + dr[i]
-                nc = c + dc[i]
-                if (0 <= nr < 5 and 0 <= nc < 5) :
-                    if place[nr][nc] == "O" and (visit[nr][nc] == -1 or visit[nr][nc] > visit[r][c]) :
-                        que.append((nr,nc))
-                        visit[nr][nc] = visit[r][c] + 1
-                    elif place[nr][nc] == "P" :
-                        if visit[r][c] == 1 :
-                            answer.append(0)
-                            brkFlag = True
-                            print(*visit)
-                            break
-                        que.append((nr,nc))
-                        visit[nr][nc] = 0
+        while que1 :
+            visit = [[-1] * 5 for _ in range(5)]
+            que2 = deque()
+            que2.append(que1.popleft())
+            visit[que2[0][0]][que2[0][1]] = 0
+            while que2 :
+                r, c = que2.popleft()
+                for i in range(4) :
+                    nr = r + dr[i]
+                    nc = c + dc[i]
+                    if (0 <= nr < 5 and 0 <= nc < 5) :
+                        if place[nr][nc] != "X" and visit[nr][nc] == -1 :
+                            que2.append((nr,nc))
+                            visit[nr][nc] = visit[r][c] + 1
+                            if place[nr][nc] == "P" and visit[nr][nc] <= 2 :
+                                answer.append(0)
+                                brkFlag = True
+                                break
+                if brkFlag : break
             if brkFlag : break
         else :
             answer.append(1)
@@ -40,9 +40,3 @@ def solution(places):
                 
     return answer
 
-
-# BFS로 탐색
-# 큐에 담을 내용
-# visit에 담을 내용
-# 하나라도 거리 2 안에있는거 발견하면 끝 (0푸쉬)
-# 처음에 P위치 초기화 필요
