ffc9c44d5b878e353dbb0665641bf6f9cc2977eb
yunchanpark <yongp99@nate.com>
2022-05-04 21:21:27 +0900

    징검다리 건너기(수정)

--- 윤찬/lv.3/징검다리건너기.py
+++ 윤찬/lv.3/징검다리건너기.py
@@ -1,21 +1,20 @@
 def solution(stones, k):
-    start = 0
-    end = max(stones)
     result = 0
-    
+    start = 0
+    end = max(stones[:k])
     while start <= end:
         mid = (start + end) // 2
         cnt = 0
-
         for stone in stones:
-            if stone - mid <= 0: cnt += 1
-            else: cnt = 0 
+            if stone - mid <= 0:
+                cnt += 1
+            else:
+                cnt = 0
             if k == cnt:
                 end = mid - 1
                 break
-            
-        if cnt < k:
+        else:
             start = mid + 1
-            result = mid
+            result = start
 
     return result
