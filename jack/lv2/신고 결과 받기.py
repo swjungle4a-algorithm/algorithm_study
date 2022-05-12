b679c4eeeb97bd43167d01400832a78288a57e53
Jack <95907671+JackLee9437@users.noreply.github.com>
2022-05-12 15:35:30 +0900

    Update 신고 결과 받기.py
    dict 형태로 수정

--- jack/lv2/신고 결과 받기.py
+++ jack/lv2/신고 결과 받기.py
@@ -1,12 +1,13 @@
 from collections import Counter
 
 def solution(id_list, report, k):
-    answer = [0] * len(id_list)
+    answer = {x : 0 for x in id_list}
     report = set(report)
     c = Counter(map(lambda x : x.split()[1], report))
     
     for rp in report :
         reporter, reportee = rp.split()
         if c[reportee] >= k :
-            answer[id_list.index(reporter)] += 1
+            answer[reporter] += 1
+    answer = list(answer.values())
     return answer
