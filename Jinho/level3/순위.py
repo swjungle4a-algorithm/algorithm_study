aa2093599365b59df8c5f429fb7aa68c797991c4
dlwlsh92 <102504924+dlwlsh92@users.noreply.github.com>
2022-05-29 19:50:36 +0900

    Update 순위.py

--- Jinho/level3/순위.py
+++ Jinho/level3/순위.py
@@ -11,28 +11,28 @@ def solution(n, results):
         reverse_sort[loser].append(winner)
     
     for num in range(1, n+1):
-
+        
         sortq = deque([num])
         rsortq = deque([num])
         
         s_set = set()
-        
+
         while sortq:
             now = sortq.popleft()
             for next in sort[now]:
                 if next not in s_set:
                     sortq.append(next)
                     s_set.add(next)
-                    cnt += 1
+                    
         while rsortq:
             now = rsortq.popleft()
             for next in reverse_sort[now]:
                 if next not in s_set:
                     rsortq.append(next)
                     s_set.add(next)
-                    cnt += 1
+                    
                 
-        if cnt == n-1:
+        if len(s_set) == n-1:
             answer += 1
             
     return answer
