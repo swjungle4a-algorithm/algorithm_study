5194043be47a3f9c42e6983503f4e913b707e2b1
jhw607 <68524314+jhw607@users.noreply.github.com>
2022-06-04 23:07:04 +0900

    Update 12936_줄서는방법_220604.py

--- 혜원/Lv2/12936_줄서는방법_220604.py
+++ 혜원/Lv2/12936_줄서는방법_220604.py
@@ -6,17 +6,16 @@ def fact(n):
 def solution(n, k):
     answer = [] 
     nums = [i for i in range(1,n+1)]
+    
     for i in range(1, n+1):
         q, k = divmod(k, fact(n-i))
         if k == 0:
             answer.append(nums.pop(q-1))            
             break            
         else:
-            answer.append(nums.pop(q))            
-        
+            answer.append(nums.pop(q))     
 
     while nums:
         answer.append(nums.pop())
-    print(answer)
 
     return answer
