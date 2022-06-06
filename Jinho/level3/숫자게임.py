6896646bd99fe7d806bda075ab0bf75c8998a38a
dlwlsh92 <102504924+dlwlsh92@users.noreply.github.com>
2022-06-06 20:54:23 +0900

    Create 숫자게임.py

---
+++ Jinho/level3/숫자게임.py
@@ -0,0 +1,17 @@
+from collections import deque
+def solution(A, B):
+    answer = 0
+    Asortq = deque(sorted(A, reverse=True))
+    Bsort = sorted(B, reverse=True)
+    while Asortq:
+        if Asortq[-1] < Bsort[-1]:
+            answer += 1
+            Asortq.pop()
+            Bsort.pop()
+        elif Asortq[-1] > Bsort[-1]:
+            Asortq.popleft()
+            Bsort.pop()
+        else:
+            Asortq.popleft()
+            Bsort.pop()
+    return answer
