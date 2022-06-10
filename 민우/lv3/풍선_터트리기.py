f181f4eed97d107d5bfe6940e4c23d5c82e001db
minuet1215 <86056489+minuet1215@users.noreply.github.com>
2022-06-10 20:23:56 +0900

    Create 풍선_터트리기.py

---
+++ 민우/lv3/풍선_터트리기.py
@@ -0,0 +1,3 @@
+def solution(a, m=1000000001, n=1000000001):
+    l, r = [(m := min(m, v)) for v in a[:-1]], [(n := min(n, v)) for v in reversed(a[2:])]
+    return sum((x > y or z > y for x, y, z in zip(l, a[1:-1], reversed(r)))) + 2
